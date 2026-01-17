082fea8a15c95adb463c0294dfd4523b0bf05e98
Rion <kanade.miyachi@gmail.com>
2026-01-17 20:00:34 +0900

    不使用宣言

--- src/main.rs
+++ src/main.rs
@@ -8,7 +8,7 @@ fn ifs(){
     println!("これはif文のべんきょーです。");
     const A:&str = "Hello";
     let mut c = 1;
-    let mut b = 2;
+    let mut _b = 2;
     if c==2{
         println!("cは2です。{A}");
     }else{
