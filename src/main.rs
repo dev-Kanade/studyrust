7c824e811a25f446486143a8a5d86ddf8a903cb1
Rion <kanade.miyachi@gmail.com>
2026-01-17 19:59:35 +0900

    s

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
 fn main() {
     println!("Hello, world!");
+    ifs();
+}
+
+
+fn ifs(){
+    println!("これはif文のべんきょーです。");
+    const A:str = "Hello";
+    let mut c = 1;
+    let mut b = 2;
+    if c==2{
+        println!("cは2です。{A}");
+    }else{
+        prinln!("{A}");
+    };
 }
